86c901e8f1153f0ffb47cc3dfd3b02d573e66fcb
niemingzhi <1094290505@qq.com>
2023-01-31 11:34:34 +0800

    测试输出流和错误流

---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,11 @@
+function exec1(){
+    alert("test1");
+    fm.action = "/testLinux";
+    fm.submit;
+}
+
+function exec2(){
+    alert("test2");
+    fm.action = "/testLinux2";
+    fm.submit;
+}
